acf25ee65b182f19508ed6f9c48de9bb2e5cb5f3
Pratyush Choudhary <solixious@gmail.com>
2026-04-20 16:08:34 +0400

    Added a basic all tools page

---
+++ src/main/java/in/nextworth/controller/ToolsController.java
@@ -0,0 +1,13 @@
+package in.nextworth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToolsController {
+
+    @GetMapping("/tools")
+    public String tools() {
+        return "tools";
+    }
+}
