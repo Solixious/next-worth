612d657695fd3952aaaf1d82b161533aa1487dab
Pratyush Choudhary <solixious@gmail.com>
2026-04-30 13:40:13 +0400

    Adding guide links to home page

--- src/main/java/in/nextworth/controller/HomeController.java
+++ src/main/java/in/nextworth/controller/HomeController.java
@@ -1,13 +1,24 @@
 package in.nextworth.controller;
 
+import in.nextworth.model.GuideMetadata;
+import in.nextworth.service.GuideService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private GuideService guideService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<GuideMetadata> guides = guideService.listGuides();
+        model.addAttribute("guides", guides);
         return "home";
     }
 }
